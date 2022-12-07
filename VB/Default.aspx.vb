7f01e25fc3299501bb6e41cc2fea9f3b0bc50074
DevExpressExampleBot <fakeEmail>
2022-12-07 15:59:19 +0400

    Source auto update [skip ci]

--- VB/Default.aspx.vb
+++ VB/Default.aspx.vb
@@ -8,7 +8,7 @@ Imports System.Web.UI
 Imports System.Web.UI.WebControls
 Imports System.Web.UI.WebControls.WebParts
 Imports System.Web.UI.HtmlControls
-Imports DevExpress.Web.ASPxClasses
+Imports DevExpress.Web
 
 Partial Public Class CookieRoutinesSample
     Inherits System.Web.UI.Page
